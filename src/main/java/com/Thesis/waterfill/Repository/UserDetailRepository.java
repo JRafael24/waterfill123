b7e3702e5a349334c52288c8170f4e9b2e52ea03
JRafael24 <rafaelbathan@gmail.com>
2024-07-22 15:43:02 +0800

    ayos na din ang profile details

--- src/main/java/com/Thesis/waterfill/Repository/UserDetailRepository.java
+++ src/main/java/com/Thesis/waterfill/Repository/UserDetailRepository.java
@@ -4,11 +4,13 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.Thesis.waterfill.Model.UserDetail;
+import java.util.List;
+
 
 
 public interface UserDetailRepository extends JpaRepository<UserDetail, Long> {
     Optional<UserDetail> findByPhoneNumber(String phoneNumber);
-    UserDetail findByUserId(Long userId);
+    UserDetail  findByUserId(Long userId);
 }
 
 
