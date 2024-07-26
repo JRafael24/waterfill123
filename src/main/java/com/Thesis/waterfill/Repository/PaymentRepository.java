e18d1215df070a717edc45b675543b0f55e903be
JRafael24 <rafaelbathan@gmail.com>
2024-07-26 11:57:38 +0800

    Payment again

--- src/main/java/com/Thesis/waterfill/Repository/PaymentRepository.java
+++ src/main/java/com/Thesis/waterfill/Repository/PaymentRepository.java
@@ -1,9 +1,12 @@
 package com.Thesis.waterfill.Repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.Thesis.waterfill.Model.*;
 
 public interface PaymentRepository extends JpaRepository <Payment, Long>{
+    List<Payment> findByUserId(Long userId);
 
 }  
